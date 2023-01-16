25f7b5ab73814ba547aef68f0d048abb1258e7dc
Liubou Grebennikova <liubou.grebennikova@gmail.com>
2023-01-16 11:18:06 +0200

    feat: some more tasks implement

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -112,8 +112,15 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function attempt() {
+    const at = attempts;
+    try {
+      return func();
+    } catch (e) {
+      return attempt(at - 1);
+    }
+  };
 }
 
 
